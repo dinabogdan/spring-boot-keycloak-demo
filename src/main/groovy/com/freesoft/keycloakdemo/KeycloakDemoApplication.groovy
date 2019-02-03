91c63e2dafe29cc85acde78a32f3763f2fef6bf1
bogdan.dina <bogdan.dina@orange.com>
2019-02-03 14:29:54 +0200

    first commit

---
+++ src/main/groovy/com/freesoft/keycloakdemo/KeycloakDemoApplication.groovy
@@ -0,0 +1,14 @@
+package com.freesoft.keycloakdemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KeycloakDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(KeycloakDemoApplication, args)
+	}
+
+}
+
