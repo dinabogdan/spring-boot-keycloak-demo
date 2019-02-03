063c198de3514d4f5b8d94d25cea0921eb3d7481
bogdan.dina <bogdan.dina@orange.com>
2019-02-03 23:05:27 +0200

    finally it's working

--- src/main/groovy/com/freesoft/keycloakdemo/SecurityConfiguration.groovy
+++ src/main/groovy/com/freesoft/keycloakdemo/SecurityConfiguration.groovy
@@ -1,16 +1,17 @@
 package com.freesoft.keycloakdemo
 
+import org.keycloak.KeycloakPrincipal
+import org.keycloak.KeycloakSecurityContext
 import org.keycloak.adapters.KeycloakConfigResolver
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver
+import org.keycloak.adapters.springboot.KeycloakSpringBootProperties
 import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.FilterType
-import org.springframework.context.annotation.Primary
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.*
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -18,12 +19,16 @@ import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper
 import org.springframework.security.core.session.SessionRegistryImpl
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy
+import org.springframework.web.context.WebApplicationContext
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+
+import java.security.Principal
 
 @Configuration
 @EnableWebSecurity
-@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class,
-        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX,
-                pattern = "org.keycloak.adapters.springsecurity.management.HttpSessionManager"))
+@EnableConfigurationProperties(KeycloakSpringBootProperties.class)
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
 
     @Autowired
@@ -53,5 +58,24 @@ class SecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
                 .hasRole("user")
                 .anyRequest()
                 .permitAll()
+    }
+
+    @Bean
+    @Scope(scopeName = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    KeycloakSecurityContext provideKeycloakSecurityContext() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes()
+        Principal principal = attributes.getRequest().getUserPrincipal()
+        if (principal == null) {
+            null
+        }
+
+        if (principal instanceof KeycloakAuthenticationToken) {
+            principal = Principal.class.cast(KeycloakAuthenticationToken.class.cast(principal).getPrincipal())
+        }
+
+        if (principal instanceof KeycloakPrincipal) {
+            KeycloakPrincipal.class.cast(principal).getKeycloakSecurityContext()
+        }
+        null
     }
 }
--- src/main/groovy/com/freesoft/keycloakdemo/WebController.groovy
+++ src/main/groovy/com/freesoft/keycloakdemo/WebController.groovy
@@ -1,11 +1,16 @@
 package com.freesoft.keycloakdemo
 
+import org.keycloak.KeycloakPrincipal
+import org.keycloak.KeycloakSecurityContext
+import org.keycloak.adapters.RefreshableKeycloakSecurityContext
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
-import java.security.Principal
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Controller
 class WebController {
@@ -17,10 +22,12 @@ class WebController {
     def index() { "external" }
 
     @GetMapping(path = "/customers")
-    String customers(Principal principal, Model model) {
+    String customers(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, KeycloakPrincipal principal, Model model) {
+        RefreshableKeycloakSecurityContext context = httpServletRequest.getAttribute(KeycloakSecurityContext.class.getName())
+        def username = context.token.preferredUsername
         Iterable<Customer> customers = customerDAO.findAll()
         model.addAttribute("customers", customers)
-        model.addAttribute("username", principal.getName())
+        model.addAttribute("username", username)
         "customers"
     }
 }
